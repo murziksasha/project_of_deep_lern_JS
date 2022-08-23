b2f56a8b7dd2931ca757fb0aa57d54ae10444f52
Alexander Grygoriev <truedevelop@yahoo.com>
2022-08-23 14:47:26 +0300

    Create mask to number of forms on site

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,20 @@
 import modals from './modules/modals.js';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
+import {checkEngTextInputs} from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  sliders('.feedback-slider-item','horicantal', '.main-prev-btn', '.main-next-btn');
-  sliders('.main-slider-item', 'vertical')
-
+  sliders('.feedback-slider-item', 'horicantal', '.main-prev-btn', '.main-next-btn');
+  sliders('.main-slider-item', 'vertical');
 
   modals();
   forms();
-
-
+  mask('[name="phone"]');
+  checkTextInputs('[name="name"]');
+  checkTextInputs('[name="message"]');
+  checkEngTextInputs('[name="email"]');
 });
