a0163f93bb0c99fa18788f9153d6da253da7b14c
Alexander Grygoriev <truedevelop@yahoo.com>
2022-08-23 12:07:58 +0300

    auto sliders scroll DONE

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 import modals from './modules/modals.js';
+import sliders from './modules/sliders';
+import forms from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  sliders('.feedback-slider-item','horicantal', '.main-prev-btn', '.main-next-btn');
+  sliders('.main-slider-item', 'vertical')
+
+
   modals();
+  forms();
+
 
 });
